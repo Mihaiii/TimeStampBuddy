3c05a62670310a36a2f26e5dd5103e47d5d08f42
Mihai Chirculescu <mihai.chirculescu@yahoo.com>
2024-12-28 17:35:23 +0200

    Bug fixes

--- youtube_id_to_timestamps.py
+++ youtube_id_to_timestamps.py
@@ -20,8 +20,9 @@ class YoutubeIdToTimestamps:
 
     def _get_transcript(self, youtube_id):
         logging.info(f"{youtube_id} - making the request to get the transcript")
-        data = YouTubeTranscriptApi.get_transcript(youtube_id)
-        logging.info(f"{youtube_id} - got the transcript")
+        #TODO: get the language from the text from the tweet
+        data = YouTubeTranscriptApi.get_transcript(youtube_id, languages=['en', 'es', 'de'])
+        logging.info(f"{youtube_id} - got the transcript. First 5 objs: {data[:5]}")
         transformed_data = [
             {
                 "text": item["text"],
@@ -70,7 +71,7 @@ class YoutubeIdToTimestamps:
             "role": "user",
             "parts": [
                 files[0],
-                "Attached you have the transcript of a YouTube video. It's a JSON file that has the following properties: text - what is said, meaning the actual transcript, start: the second of that fragment in the video.\n\nI want you to make a summary of the video based on that transcript data and also include the timestamps (make sure they are in HH:MM:SS format), meaning between what hour, minute and second in the video a generated topic/idea is mentioned. Make the topics short. Have lots of topics. Have one topic per line and each line starts with the timestamps.\n\nMake sure the generated topics are in the same language as the initial text.\n\nHere is an example of how your response look like. Pay attention to the format. This is the summary for another video:\n\n0:00 - Introduction\n2:03 - Startup philosophy\n9:34 - Low points\n13:03 - 12 startups in 12 months\n19:55 - Traveling and depression\n32:34 - Indie hacking\n36:37 - Photo AI\n1:12:53 - How to learn AI\n1:21:30 - Robots\n1:29:47 - Hoodmaps\n1:53:52 - Learning new programming languages\n2:03:24 - Monetize your website\n2:09:59 - Fighting SPAM\n2:13:33 - Automation\n2:24:58 - When to sell startup\n2:27:52 - Coding solo\n2:33:54 - Ship fast\n2:42:38 - Best IDE for programming\n2:52:09 - Andrej Karpathy\n3:01:34 - Productivity\n3:15:21 - Minimalism\n3:24:07 - Emails\n3:31:20 - Coffee\n3:39:05 - E/acc\n3:41:21 - Advice for young people\n\nDon't make it too granular. Extract the main ideas/chapters and present them. Only have a chapter at every few minutes, like in the example. Mention as timestamp the beginning of each chapter. See the provided example from above for a better understanding.\n",
+                "Attached you have the transcript of a YouTube video. It's a JSON file that has the following properties: text - what is said, meaning the actual transcript, start: the second of that fragment in the video.\n\nI want you to make a summary of the video based on that transcript data and also include the timestamps (make sure they are in HH:MM:SS format), meaning between what hour, minute and second in the video a generated topic/idea is mentioned. Make the topics short. Have lots of topics. Have one topic per line and each line starts with the timestamps.\n\nHere is an example of how your response look like. Pay attention to the format. This is the summary for another video:\n\n0:00 - Introduction\n2:03 - Startup philosophy\n9:34 - Low points\n13:03 - 12 startups in 12 months\n19:55 - Traveling and depression\n32:34 - Indie hacking\n36:37 - Photo AI\n1:12:53 - How to learn AI\n1:21:30 - Robots\n1:29:47 - Hoodmaps\n1:53:52 - Learning new programming languages\n2:03:24 - Monetize your website\n2:09:59 - Fighting SPAM\n2:13:33 - Automation\n2:24:58 - When to sell startup\n2:27:52 - Coding solo\n2:33:54 - Ship fast\n2:42:38 - Best IDE for programming\n2:52:09 - Andrej Karpathy\n3:01:34 - Productivity\n3:15:21 - Minimalism\n3:24:07 - Emails\n3:31:20 - Coffee\n3:39:05 - E/acc\n3:41:21 - Advice for young people\n\nDon't make it too granular. Extract the main ideas/chapters and present them. Only have a chapter at every few minutes, like in the example. Mention as timestamp the beginning of each chapter. See the provided example from above for a better understanding.\n",
             ]
         }
 
@@ -84,7 +85,7 @@ class YoutubeIdToTimestamps:
             }
         )
 
-        follow_up_message = "That's good, but it's too granular. Extract the main ideas/chapters and present them. Only have a chapter at every few minutes, like in the example. Mention as timestamp the beginning of each chapter. See the provided example from above for a better understanding."
+        follow_up_message = "That's good, but it's too granular. Extract the main ideas/chapters and present them. Only have a chapter at every few minutes, like in the example. Mention as timestamp the beginning of each chapter. See the provided example from above for a better understanding. Answer only with the timestamps and chapters, nothing else."
         response = chat_session.send_message(follow_up_message)
-        logging.info(f"{youtube_id} - {response}")
+        logging.info(f"{youtube_id} - {response.text}")
         return response.text
